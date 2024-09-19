054c21ba2f25719d64969219aabd851513451c0e
abdialimovtemur <abdialimovtemur@gmail.com>
2024-09-19 11:35:47 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+const navbar = document.querySelector(".navbar");
+const url = "https://food-pos-data.vercel.app/";
+let path = null;
+const cards = document.querySelector(".cards");
+let cardsUrl = null;
+
+
+const navRender = (data) => {
+    navbar.innerHTML = data
+        .map(
+            (item) => `
+        <div class="text-white" data-path="${item.path}">
+           <h1>${item.name}</h1>
+        </div>
+    `
+        )
+        .join("");
+};
+
+
+const cardsRender = (cardsData) => {
+    cards.innerHTML = cardsData
+        .map(
+            (item) => `
+        
+
+    `
+        )
+        .join("");
+};
+
+
+const getCardsData = async () => {
+    if (!cardsUrl) return;
+
+    try {
+        const res = await fetch(cardsUrl);
+        const cardsData = await res.json();
+        cardsRender(cardsData);
+
+        console.log(cardsData);
+    } catch (error) {
+        console.log("Error", error);
+    }
+};
+
+
+const getData = async () => {
+    try {
+        const res = await fetch(`${url}catalog`);
+        const data = await res.json();
+        navRender(data);
+
+        console.log(data);
+    } catch (error) {
+        console.log("Error", error);
+    }
+};
+getData();
+
+
+navbar.addEventListener("click", (e) => {
+    const targetElement = e.target.closest("[data-path]");
+    if (targetElement) {
+        path = targetElement.dataset.path;
+        cardsUrl = `${url}${path}`;
+        console.log(cardsUrl);
+
+        getCardsData();
+    }
+});
+
+
+
